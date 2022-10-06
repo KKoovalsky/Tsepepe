bfec58884ad97ed167b61d8d3812e78cf8efade8
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-10-06 11:05:58 +0200

    Add initial tests of the finder tool

--- tests/paired_cpp_file_finder/features/steps/steps.py
+++ tests/paired_cpp_file_finder/features/steps/steps.py
@@ -1,5 +1,9 @@
 import os
+import subprocess
 from pathlib import Path
+from hamcrest import assert_that, equal_to, empty, not_
+from features.helpers.utils import get_tool_path, get_result
+from features.helpers.tool_result import ToolResult
 
 
 @given("C++ file under path {path}")
@@ -10,14 +14,22 @@ def step_impl(context, path: str):
     abs_path = os.path.join(context.working_directory, normalized_path)
     directory = os.path.dirname(abs_path)
     Path(directory).mkdir(parents=True, exist_ok=True)
-    open(abs_path, 'w')
+    open(abs_path, "w")
 
 
 @when("Paired file for {path} is searched")
 def step_impl(context, path: str):
-    raise NotImplementedError("STEP: When Paired file for dir/file1.hpp is searched")
+    tool_path = get_tool_path(context)
+    project_root_dir = context.working_directory
+    cmd = [tool_path, project_root_dir, path]
+    cmd_result = subprocess.run(cmd, capture_output=True)
+    context.result = ToolResult(
+        cmd_result.stdout, cmd_result.stderr, cmd_result.returncode
+    )
 
 
 @then("Finding result is {path}")
 def step_impl(context, path: str):
-    raise NotImplementedError("STEP: Then Finding result is dir/file1.cpp")
+    expected_stdout = os.path.join(context.working_directory, path)
+    stdout = get_result(context).stdout.rstrip()
+    assert_that(stdout, equal_to(expected_stdout))
