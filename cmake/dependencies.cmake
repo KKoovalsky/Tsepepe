94c38e5d2acb58f592c29ab8923bf240b90e1287
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-02 11:14:36 +0100

    Get rid of Cucumber CPP
    This is to flaky.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -20,30 +20,4 @@ function(ProvideNamedType)
 
 endfunction()
 
-function(ProvideCucumberCpp)
-
-    option(CUKE_ENABLE_BOOST_TEST   "Enable Boost.Test framework for Cucumber-CPP" ON)
-    option(CUKE_USE_STATIC_BOOST    "Statically link Boost (except boost::test) with Cucumber-CPP" OFF)
-    option(CUKE_USE_STATIC_GTEST    "Statically link Google Test with Cucumber-CPP" OFF)
-    option(CUKE_TESTS_E2E           "Enable end-to-end tests for Cucumber-CPP" OFF)
-    option(CUKE_TESTS_UNIT          "Enable unit tests for Cucumber-CPP" OFF)
-    option(CUKE_ENABLE_GTEST        "Enable Google Test framework for Cucumber-CPP" OFF)
-    option(CUKE_ENABLE_QT           "Enable Qt framework for Cucumber-CPP" OFF)
-
-    set(patch ${TSEPEPE_CMAKE_DIR}/0001_cucumber_cpp_fix_asio_std_exchange_error.patch)
-    FetchContent_Declare(
-        CucumberCpp
-        GIT_REPOSITORY https://github.com/cucumber/cucumber-cpp.git
-        GIT_TAG 15d73be1759d920206907793720d1e30902428df
-        # We need this patch step, because we need Boost as well in Tsepepe project, and it leads to some weird
-        # behavior when resolving includes.
-        # The patch step will try to apply the patch, and in case it fails it will try to check if the reversed patch
-        # applies. If it does, then we know that the patch is correctly applied, thus the step is successful.
-        PATCH_COMMAND git apply ${patch} || git apply ${patch} --reverse --check
-    )
-    FetchContent_MakeAvailable(CucumberCpp)
-
-endfunction()
-
 include(FetchContent)
-set(TSEPEPE_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
