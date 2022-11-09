fffb178b8ae5bef894ba8317b00628fab2d710bf
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-09 09:42:56 +0100

    Document suitable place in class finder

--- src/include/libclang_utils/suitable_place_in_class_finder.hpp
+++ src/include/libclang_utils/suitable_place_in_class_finder.hpp
@@ -21,6 +21,17 @@ struct SuitablePublicMethodPlaceInCppFile
     auto operator<=>(const SuitablePublicMethodPlaceInCppFile&) const = default;
 };
 
+/**
+ * @brief Finds a suitable place in class to put a public method.
+ *
+ * It analyzes the C++ file to find a proper place where a new public method may be put. Normally, it will return the
+ * line which is just after the other explicit public methods, which are already declared, or defined. If no public
+ * method is declared, or defined, then the first 'public' section line will be chosen. It means that it will be the
+ * first line below the 'public:' access specifier. For structs, it will be the first line just after the body opening
+ * '{' bracket. In case, no public section is defined within the class, then in the result the
+ * SuitablePublicMethodPlaceInCppFile::is_public_section_needed will be set to true. This indicates that the class needs
+ * to have 'public:' section added, just below the SuitablePublicMethodPlaceInCppFile::line number.
+ */
 SuitablePublicMethodPlaceInCppFile find_suitable_place_in_class_for_public_method(const std::string& cpp_file_content,
                                                                                   const clang::CXXRecordDecl*,
                                                                                   const clang::SourceManager&);
