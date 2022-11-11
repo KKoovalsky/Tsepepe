4700911b06a38cf86ff8418dd20635a26ec0a30a
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-11 15:55:18 +0100

    Add ostream overload for CodeInsertion
    It must be included before Catch2 headers to make it work properly.

---
+++ tests/catch2/ostream_printers.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file        ostream_printers.hpp
+ * @brief       ostream operator overloads for the custom types.
+ */
+#ifndef OSTREAM_PRINTERS_HPP
+#define OSTREAM_PRINTERS_HPP
+
+#include <ostream>
+
+#include "common_types.hpp"
+
+namespace Tsepepe
+{
+
+inline std::ostream& operator<<(std::ostream& os, const Tsepepe::CodeInsertion& code_insertion)
+{
+    return os << "Code: \"" << code_insertion.code << "\", line: " << code_insertion.line
+              << ", column: " << code_insertion.column;
+}
+
+} // namespace Tsepepe
+
+#endif /* OSTREAM_PRINTERS_HPP */
