b7cc4e6d66ac87402533e6cd1631e16ef3955aff
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-11 16:12:51 +0100

    Reuse the scope remover, thus be DRY

--- src/src/libclang_utils/pure_virtual_functions_extractor.cpp
+++ src/src/libclang_utils/pure_virtual_functions_extractor.cpp
@@ -8,45 +8,32 @@
 #include "libclang_utils/full_function_declaration_expander.hpp"
 #include "libclang_utils/pure_virtual_functions_extractor.hpp"
 
+#include "scope_remover.hpp"
+
 using namespace clang;
+using namespace Tsepepe;
 
 // --------------------------------------------------------------------------------------------------------------------
 // Private declarations
 // --------------------------------------------------------------------------------------------------------------------
-static std::string shortify(std::string shortified, std::string shorting);
-
-/**
- * @brief Creates all the nesting scopes for the fully qualified name.
- *
- * For example, for a fully qualified name "SomeNamespace::SomeTopLevelClass::SomeNestedClass" will return a vector of
- * values:
- *              - SomeNamespace::SomeTopLevelClass::SomeNestedClass::
- *              - SomeNamespace::SomeTopLevelClass::
- *              - SomeNamespace::
- */
-static std::vector<std::string> make_nesting_scopes(const std::string& fully_qualified_name);
 
 // --------------------------------------------------------------------------------------------------------------------
 // Public stuff
 // --------------------------------------------------------------------------------------------------------------------
-Tsepepe::OverrideDeclarations
+OverrideDeclarations
 Tsepepe::pure_virtual_functions_to_override_declarations(const clang::CXXRecordDecl* node,
                                                          std::string implementor_fully_qualified_name,
                                                          const clang::SourceManager& source_manager)
 {
     OverrideDeclarations override_declarations;
-    auto implementor_nesting_scopes{make_nesting_scopes(implementor_fully_qualified_name)};
+    AllScopeRemover implementor_scopes_remover{FullyQualifiedName{implementor_fully_qualified_name}};
 
     auto append_override_declaration{[&](const CXXMethodDecl* method) {
+        const auto& interface_name{method->getParent()->getQualifiedNameAsString()};
+
         auto declaration{Tsepepe::fully_expand_function_declaration(method, source_manager)};
-
-        auto interface_nesting_prefix{method->getParent()->getQualifiedNameAsString() + "::"};
-        declaration = shortify(declaration, interface_nesting_prefix);
-
-        auto implementer_nesting_prefix{node->getQualifiedNameAsString()};
-        for (const auto& scope : implementor_nesting_scopes)
-            declaration = shortify(declaration, scope);
-
+        declaration = ScopeRemover{FullyQualifiedName{interface_name}}.remove_from(declaration);
+        declaration = implementor_scopes_remover.remove_from(declaration);
         declaration.append(" override;");
 
         override_declarations.emplace_back(std::move(declaration));
@@ -70,29 +57,3 @@ Tsepepe::pure_virtual_functions_to_override_declarations(const clang::CXXRecordD
 // --------------------------------------------------------------------------------------------------------------------
 // Private definitions
 // --------------------------------------------------------------------------------------------------------------------
-static std::string shortify(std::string shortified, std::string shorting)
-{
-    return std::regex_replace(shortified, std::regex{shorting}, "");
-}
-
-static std::vector<std::string> make_nesting_scopes(const std::string& fully_qualified_name)
-{
-    std::vector<std::string> result;
-    result.reserve(4);
-
-    auto actual_begin{std::begin(fully_qualified_name)};
-
-    auto it{std::rbegin(fully_qualified_name)};
-    auto end{std::rend(fully_qualified_name)};
-    while (it != end)
-    {
-        result.emplace_back(std::string(actual_begin, it.base()) + "::");
-        it = std::find(it, end, ':');
-        if (it == end)
-            break;
-        // Skip the "::"
-        it += 2;
-    }
-
-    return result;
-}
