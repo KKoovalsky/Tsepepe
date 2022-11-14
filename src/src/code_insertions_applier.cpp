cc47b81dde4137c130190c171c7eb6ca5dd39c67
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-14 22:18:29 +0100

    Implement code insertion applier

--- src/src/code_insertions_applier.cpp
+++ src/src/code_insertions_applier.cpp
@@ -4,12 +4,71 @@
  */
 
 #include "code_insertions_applier.hpp"
+#include "base_error.hpp"
 
 #include <algorithm>
+#include <numeric>
+#include <string>
 
+// --------------------------------------------------------------------------------------------------------------------
+// Private types and aliases
+// --------------------------------------------------------------------------------------------------------------------
+using CodeInsertions = std::vector<Tsepepe::CodeInsertionByOffset>;
+
+// --------------------------------------------------------------------------------------------------------------------
+// Private declarations
+// --------------------------------------------------------------------------------------------------------------------
+static void validate_insertions_in_bounds(const std::string& input, const CodeInsertions&);
+
+// --------------------------------------------------------------------------------------------------------------------
+// Public stuff
+// --------------------------------------------------------------------------------------------------------------------
 std::string Tsepepe::apply_insertions(const std::string& input, std::vector<CodeInsertionByOffset> insertions)
 {
+    validate_insertions_in_bounds(input, insertions);
+
+    auto [new_insertions_end_it, insertions_end_it] = std::ranges::remove_if(
+        insertions, [](const CodeInsertionByOffset& insertion) { return insertion.code.empty(); });
+    insertions.erase(new_insertions_end_it, insertions_end_it);
+
     std::ranges::sort(insertions, [](const auto& l, const auto& r) { return l.offset < r.offset; });
 
-    return {};
+    std::vector<std::string_view> chunks;
+    chunks.reserve(insertions.size() + 1);
+
+    auto chunk_begin_it{std::begin(input)};
+    for (const auto& insertion : insertions)
+    {
+        auto chunk_end_it{std::next(std::begin(input), insertion.offset)};
+        chunks.emplace_back(std::string_view(chunk_begin_it, chunk_end_it));
+        chunk_begin_it = chunk_end_it;
+    }
+    chunks.emplace_back(std::string_view(chunk_begin_it, std::end(input)));
+
+    auto insertions_total_size{std::accumulate(
+        std::begin(insertions), std::end(insertions), 0u, [](auto sum, const CodeInsertionByOffset& insertion) {
+            return sum + insertion.code.size();
+        })};
+
+    std::string result;
+    result.reserve(insertions_total_size + input.size());
+    for (unsigned i{0}; i < insertions.size(); ++i)
+    {
+        result += chunks[i];
+        result += std::move(insertions[i].code);
+    }
+    result += chunks.back();
+
+    return result;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Private definitions
+// --------------------------------------------------------------------------------------------------------------------
+static void validate_insertions_in_bounds(const std::string& input, const CodeInsertions& insertions)
+{
+    for (const auto& insertion : insertions)
+        if (input.size() < insertion.offset)
+            throw Tsepepe::BaseError{"Code insertion: \"" + insertion.code + "\", at offset "
+                                     + std::to_string(insertion.offset) + ", out of bounds"};
 }
--- tests/catch2/test_code_insertions_applier.cpp
+++ tests/catch2/test_code_insertions_applier.cpp
@@ -3,7 +3,9 @@
  * @brief       Tests applying of the code insertions.
  */
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_exception.hpp>
 
+#include "base_error.hpp"
 #include "code_insertions_applier.hpp"
 
 using namespace Tsepepe;
@@ -25,19 +27,66 @@ TEST_CASE("Code insertions are applied", "[CodeInsertionApplier]")
                 == "yolo basta? bang! szasta :) bum!");
     }
 
-    SECTION("Skips empty code insertions")
+    SECTION("Applies unsorted code insertions")
     {
+        std::string input{"SFINAE"};
+        std::vector<CodeInsertionByOffset> insertions{{.code = "rror!", .offset = 6},
+                                                      {.code = "n ", .offset = 5},
+                                                      {.code = "ubstitution ", .offset = 1},
+                                                      {.code = "s ", .offset = 3},
+                                                      {.code = "ailure ", .offset = 2},
+                                                      {.code = "ot ", .offset = 4}};
+        REQUIRE(apply_insertions(input, std::move(insertions)) == "Substitution Failure Is Not An Error!");
     }
 
-    SECTION("Raises error if one code insertion is out of bound")
+    SECTION("Skips empty code insertions")
     {
+        std::string input{"SFINAE"};
+        std::vector<CodeInsertionByOffset> insertions{{.code = "rror!", .offset = 6},
+                                                      {.code = "n ", .offset = 5},
+                                                      {.code = "", .offset = 4},
+                                                      {.code = "ubstitution ", .offset = 1},
+                                                      {.code = "s ", .offset = 3},
+                                                      {.code = "", .offset = 1},
+                                                      {.code = "ailure ", .offset = 2},
+                                                      {.code = "", .offset = 2},
+                                                      {.code = "ot ", .offset = 4}};
+        REQUIRE(apply_insertions(input, std::move(insertions)) == "Substitution Failure Is Not An Error!");
     }
 
     SECTION("Handles code insertions, which put stuff in the same place")
     {
+        std::string input{"Hello world!"};
+        std::vector<CodeInsertionByOffset> insertions{{.code = ", I love you", .offset = 5},
+                                                      {.code = ", do you love me", .offset = 5},
+                                                      {.code = ", or does he,", .offset = 5}};
+        REQUIRE(apply_insertions(input, std::move(insertions))
+                == "Hello, I love you, do you love me, or does he, world!");
     }
 
     SECTION("Applies code insertion exactly at the end")
     {
+        std::string input{"Hallochen"};
+        std::vector<CodeInsertionByOffset> insertions{
+            {.code = " my", .offset = 9}, {.code = " beautiful", .offset = 9}, {.code = " world!", .offset = 9}};
+        REQUIRE(apply_insertions(input, std::move(insertions)) == "Hallochen my beautiful world!");
+    }
+
+    SECTION("Applies code insertion exactly at the beginning")
+    {
+        std::string input{"World!"};
+        std::vector<CodeInsertionByOffset> insertions{
+            {.code = "Hey, ", .offset = 0}, {.code = "you. ", .offset = 0}, {.code = "Dummy. ", .offset = 0}};
+        REQUIRE(apply_insertions(input, std::move(insertions)) == "Hey, you. Dummy. World!");
+    }
+
+    SECTION("Raises error if one code insertion is out of bound")
+    {
+        std::string input{"World!"};
+        std::vector<CodeInsertionByOffset> insertions{
+            {.code = "Hey, ", .offset = 0}, {.code = "you. ", .offset = 7}, {.code = "Dummy. ", .offset = 0}};
+        REQUIRE_THROWS_AS(apply_insertions(input, insertions), Tsepepe::BaseError);
+        REQUIRE_THROWS_WITH(apply_insertions(input, std::move(insertions)),
+                            "Code insertion: \"you. \", at offset 7, out of bounds");
     }
 }
