ff1ec5da26607eefcf472568b58f4422ea1d499c
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-11-24 12:07:20 +0100

    Add number parser and validator for cmd

--- src/utils/cmd_utils.cpp
+++ src/utils/cmd_utils.cpp
@@ -3,7 +3,12 @@
  * @brief	Command Line utilities definition.
  */
 
+#include <algorithm>
+#include <cctype>
 #include <cstring>
+#include <string>
+
+#include "error.hpp"
 
 namespace Tsepepe::utils::cmd
 {
@@ -19,6 +24,14 @@ bool is_command_help_requested(int argc, const char** argv)
             return true;
 
     return false;
+}
+
+int parse_and_validate_number(const char* arg)
+{
+    std::string number_str{arg};
+    if (not std::ranges::all_of(number_str, [](unsigned char c) { return std::isdigit(c); }))
+        throw Tsepepe::Error{"Argument: " + number_str + " is not numeric!"};
+    return std::stoi(number_str);
 }
 
 } // namespace Tsepepe::utils::cmd
--- src/utils/cmd_utils.hpp
+++ src/utils/cmd_utils.hpp
@@ -10,5 +10,7 @@ namespace Tsepepe::utils::cmd
 
 bool is_command_help_requested(int argc, const char** argv);
 
-}
+int parse_and_validate_number(const char* arg);
+
+} // namespace Tsepepe::utils::cmd
 #endif /* CMD_UTILS_HPP */
